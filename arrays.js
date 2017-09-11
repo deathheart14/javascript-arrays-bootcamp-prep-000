d72c10023eecdd934849785c38168b3eac11df0d
Jason Barnes <deathheart14@yahoo.com>
2017-09-11 16:43:29 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,1 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
-
-function addElementToBeginningOfArray(choclateBar, name) {
-var newarray =[`${array}`...`${element}`]
-return newarray[]
-}
